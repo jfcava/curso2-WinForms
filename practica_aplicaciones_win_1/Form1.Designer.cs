c64c03e1b83a9b947dcca63742a9736c68d60f1b
jfcava <juancavallieri@gmail.com>
2022-08-24 01:33:07 -0300

    C# - WINFORMS - Guia de Ejercicios

---
+++ practica_aplicaciones_win_1/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace practica_aplicaciones_win_1
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmInicio));
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnInicio.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnInicio.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnInicio.FlatAppearance.BorderSize = 5;
+            this.btnInicio.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnInicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInicio.Image = ((System.Drawing.Image)(resources.GetObject("btnInicio.Image")));
+            this.btnInicio.Location = new System.Drawing.Point(184, 120);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(422, 255);
+            this.btnInicio.TabIndex = 0;
+            this.btnInicio.UseVisualStyleBackColor = false;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTitulo.Font = new System.Drawing.Font("Gadugi", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.SystemColors.Info;
+            this.lblTitulo.Location = new System.Drawing.Point(273, 45);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(250, 26);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "CONTROLA TUS AHORROS";
+            this.lblTitulo.MouseLeave += new System.EventHandler(this.lblTitulo_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(415, 430);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tu Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtNombre.Location = new System.Drawing.Point(484, 427);
+            this.txtNombre.MaxLength = 10;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(122, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 544);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btnInicio);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmInicio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ejercicio1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmInicio_FormClosed);
+            this.Load += new System.EventHandler(this.frmInicio_Load);
+            this.Click += new System.EventHandler(this.frmInicio_Click);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmInicio_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+    }
+}
+
---
+++ practica_aplicaciones_win_1/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica_aplicaciones_win_1
+{
+    public partial class frmInicio : Form
+    {
+        public frmInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void frmInicio_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bienvenido a C#");
+        }
+
+        private void frmInicio_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show("Chau chau...");
+        }
+
+        
+        
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            // Mostramos un Mensaje con MessageBox.Show.
+            //MessageBox.Show("Se disparo el evento Click", "Atención");
+
+            // Al dispararse el evento Click, se cambie el color del Formulario. 
+            //this.BackColor = Color.PapayaWhip;
+
+            // En el evento Click del botón creado anteriormente, cancelamos las líneas de código
+            // anteriores y colocamos el código para cambiar el color de fondo del TextBox
+            // (Propiedad BackColor) si el TextBox se encuentra vació.
+
+            if(txtNombre.Text == "")
+            {
+                txtNombre.BackColor = Color.Red;
+            }else
+            {
+                txtNombre.BackColor = System.Drawing.SystemColors.Control;
+            }
+        }
+
+
+        // Manejar el evento Click del Formulario. Determinar que botón del Mouse se pulso.
+        private void frmInicio_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs evento = (MouseEventArgs)e;
+
+            if(evento.Button == MouseButtons.Left)
+            {
+                MessageBox.Show("Apretaste el click izquierdo!");
+            }else if(evento.Button == MouseButtons.Right)
+            {
+                MessageBox.Show("Apretaste el click derecho!");
+            }else
+            {
+                MessageBox.Show("Apretaste el click del medio!");
+            }
+        }
+
+        // Manejar el evento MouseMove del Control Label. En la ventana de eventos elijo
+        // MouseMove.
+        // Cambiamos el color de la propiedad BackColor.
+
+        private void frmInicio_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblTitulo.BackColor = Color.BlueViolet;
+
+            // Cambiar el estilo del cursor al dispararse los eventos MouseMove y MouseLeave.    
+            lblTitulo.Cursor = Cursors.Hand;
+        }
+
+        // Al dispararse el evento MouseLeave, se restablezca el color de fondo de la etiqueta. 
+        private void lblTitulo_MouseLeave(object sender, EventArgs e)
+        {
+            lblTitulo.BackColor = System.Drawing.SystemColors.Window;
+            // Cambiar el estilo del cursor al dispararse los eventos MouseMove y MouseLeave.           
+            lblTitulo.Cursor = Cursors.Arrow;
+        }
+
+        
+    }
+}
